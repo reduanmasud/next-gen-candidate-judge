943a8911b0e43b671b9b1d540d97394789593cee
Reduan Masud <reduanmasudcse@gmail.com>
2025-11-11 19:58:28 +0000

    Pusher added with soketi

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,11 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import { configureEcho } from '@laravel/echo-react';
+
+configureEcho({
+    broadcaster: 'pusher',
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
